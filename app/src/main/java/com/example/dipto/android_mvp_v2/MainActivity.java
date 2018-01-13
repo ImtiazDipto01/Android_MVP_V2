1fb4dec2bc39480adb9ef759a9489acb4fd702f4
imtiaz <programmer.imtiaz@gmail.com>
2018-01-13 16:24:22 +0600

    ADD- added Butterknife Injection

--- app/src/main/java/com/example/dipto/android_mvp_v2/MainActivity.java
+++ app/src/main/java/com/example/dipto/android_mvp_v2/MainActivity.java
@@ -1,13 +1,62 @@
 package com.example.dipto.android_mvp_v2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity implements MainActivityView {
+
+    @BindView(R.id.textViewIp)
+    TextView textViewIp;
+    @BindView(R.id.textViewCountry)
+    TextView textViewCountry;
+    @BindView(R.id.textViewLocation)
+    TextView textViewLocation;
+    @BindView(R.id.button)
+    Button button;
+    @BindView(R.id.progressBar)
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    public void showIpInfo(IPResponse ipResponse) {
+
+    }
+
+    @Override
+    public void startLoading() {
+
+    }
+
+    @Override
+    public void stopLoading() {
+
+    }
+
+    @Override
+    public void showMessage(String msg) {
+
+    }
+
+    @Override
+    public Context getAppContext() {
+        return getApplicationContext();
+    }
+
+    @OnClick(R.id.button)
+    public void onViewClicked() {
     }
 }
